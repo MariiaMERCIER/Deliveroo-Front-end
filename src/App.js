dbb195e998d0623d434cb48f41450ce148b4dadb
Mariia IVANCHENKO <mariiaivanchenko@mbp-de-mariia.home>
2022-11-07 15:31:54 +0100

    maj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return (
+const App = () => {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:3000/");
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect =
+    (() => {
+      fetchData();
+    },
+    []);
+  return isLoading ? (
     <div className="App">
-      <h1>Hello world</h1>
+      <div className="container">
+        <header>
+          <h1>DELIVEROO</h1>
+        </header>
+        <main>
+          <span>En cours de chargement</span>
+        </main>
+      </div>
+    </div>
+  ) : (
+    <div className="App">
+      <div className="container">
+        <header>
+          <h1>DELIVEROO</h1>
+        </header>
+        <main>
+          <h2>{data.restaurant.name}</h2>
+        </main>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
